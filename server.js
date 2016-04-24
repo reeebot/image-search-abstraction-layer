e50a9e5a08ea81766015502c5305185a6c0cf9d0
Tim Meijome <tim.meijome@gmail.com>
2016-04-24 19:51:31 +0000

    rename

--- server.js
+++ server.js
@@ -1,63 +1,89 @@
 var express = require('express')
 var app = express()
 var url = require('url')
-var validator = require('validator');
 var port = process.env.PORT || 8080;
-var Input = require('./mongo-input')
-var Query = require('./mongo-query')
+var mongo = require('mongodb').MongoClient;
+var mongourl = 'mongodb://reeebot-image-search-abstraction-layer-2987352:27017';
 
-app.route('/').get(function (req, res) {	        	////// serve static index.html
+////// BING IMG API account setup
+var acctKey = 'Qd4xhMDeQE2df0oLuIeJfgh7eAaonO077gqpOhQY+Mc';
+var rootUri = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/Image';
+var auth    = new Buffer([ acctKey, acctKey ].join(':')).toString('base64');
+var request = require('request').defaults({
+  headers : {
+    'Authorization' : 'Basic ' + auth
+  }
+});
+
+////// serve static index.html
+app.route('/').get(function (req, res) {
 	res.sendFile(process.cwd()+'/public/index.html');
 });
 
-app.get('/new/*', function(req, res) {
-    var requrl = url.parse(req.url).pathname;               	// get request pathname
-    var cleanreq = requrl.replace(/^\//g, '');          	    // remove first forward slash/
-    var split = cleanreq.split(/\/(.+)/, 2);
-    if (validator.isURL(split[1])){                             // checks if valid URL format
-            var input = new Input(split, function(cb){          // input to database the original_URL
-                var jsonresponse = {                            // setup display data (original and short urls)
-                    original_url : split[1],                    // (if URL exists in database it returns stored shortURL, or creates new shortURL)
-                    short_url : 'http://'+req.headers.host+'/'+cb
-                }
-                res.send(JSON.stringify(jsonresponse))
-            });
-        }
-        else {                                                  // invalid URL format
-            var jsonresponse = {
-                error : "Invalid URL Format."
-            }
-            res.send(JSON.stringify(jsonresponse))
-        }
-})
-
-
-app.get('/*', function(req, res) {                      ////// compute the request
-    var requrl = url.parse(req.url).pathname;               	// get request pathname
-    var cleanreq = requrl.replace(/^\//g, '');          	    // remove first forward slash/
-    var split = cleanreq.split(/\/(.+)/, 2);	                // split into array of two after the first forward slash/ if present
-
-    if (Number.isInteger(+split[0])) {	        	    ////// the request is a number
-        var query = new Query(split, function(cb){              // query database with request
-            if (cb[0] === undefined) {                          // if request doesn't exist
-                var jsonresponse = {
-                    error : "This URL is not in the database."
-                }
-                res.send(JSON.stringify(jsonresponse))
-            }
-            else {                                              // if request exists, redirect to stored url
-                res.redirect(cb[0].original_url)
-            }
-        })
+////// image search
+app.get('/imgsearch/:search', function(req, res) {
+  var pagenum = 0
+  if (url.parse(req.url).query){                                // checks if a query was sent with search request (page number)
+    var splitoffset = url.parse(req.url).query.split(/=/g, 2)   // split query at =
+    if (splitoffset[0] === "offset") {                          // set the page number
+      pagenum = +splitoffset[1] * 10
     }
-    else {									        	////// the request is invalid
-    	var jsonresponse = {
-    		error : "Invalid request. Please check syntax and try again."
-    	}
-    	res.send(JSON.stringify(jsonresponse))
+  }
+  var searchterm = req.params.search;  // use :search at the top to capture the entry and req.params.search here
+  request.get({                        // send the search request to BING IMG API
+    url : rootUri,
+    qs  : {
+      $format : 'json',
+      Query   : "'" + searchterm + "'",
+      $skip   : pagenum,
+      $top    : 10
     }
+  }, function(err, response, body) {
+    if (err)
+      return res.status(500, err.message);
+    if (response.statusCode !== 200)
+      return res.status(500, response.body);
+    var results = JSON.parse(response.body);
+    var arr = []
+    for (var id in results.d.results) {       // goes through search results and pulls selected fields into new array
+      arr.push({ url : results.d.results[id].MediaUrl,
+      snippet : results.d.results[id].Title,
+      thumbnail : results.d.results[id].Thumbnail.MediaUrl,
+      context : results.d.results[id].SourceUrl
+      })}
+    res.send(arr)
+    //// save the searchterm and searchtime to db for history tracking
+    var data = {                      // setup the input data for db
+      search : searchterm,
+      time : new Date().toISOString() // grab current time
+    }
+      mongo.connect(mongourl, function(err, db) {
+        if (err) throw err
+        var collection = db.collection('imagesearch')
+        var insert = collection.insert(data, function (err, data){       // insert data into database
+          if (err) throw err;
+          db.close()
+        });
+      })
+  });
 });
 
-app.listen(port, function() {                           ////// start server
+////// display history
+app.get('/history', function(req, res) {
+  mongo.connect(mongourl, function(err, db) {
+    if (err) throw err
+    var collection = db.collection('imagesearch')
+    var findhistory = collection.find( {},{ _id : 0 } ).sort({_id:-1}).limit(10)
+    findhistory.toArray(function(err, data) {
+      if (err) throw err;
+      db.close(function(){
+        res.send(data)
+      })
+    })
+  })
+})
+
+////// start server
+app.listen(port, function() {
 	console.log('server listening')
 })
